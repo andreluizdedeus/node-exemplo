80248598d11a6fc441ce2f06ad08b2d37c7cba87
ANDRE LUIZ DE DEUS <andre.ldeus@senacsp.edu.br>
2023-11-08 14:56:41 -0300

    configuração do ts

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import validator from 'validator'
+
+console.log(validator.isEmail ('andreluizdedeuss@gmail.com'))
+
+let nome: string = "joão"
+let idade: number = 18
+
+console.log(`Meu nome é ${nome} e eu tenho ${idade} anos`) 
+
